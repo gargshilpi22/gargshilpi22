3e3114aa9ad71db8054883f17ec4d16d69b37ce3
shilpi <gargshilpi22@gmail.com>
2013-04-23 21:14:22 -0500

    adding boilerplate code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gargshilpi22::Application.config.secret_token = 'f92123fe378234c25c15508308356e54e1deee458be6c06142060e7a55c5c8bc289db600677823d1ef1ec3afc97fbe800911a265001a6afd39089d45980295e1'
